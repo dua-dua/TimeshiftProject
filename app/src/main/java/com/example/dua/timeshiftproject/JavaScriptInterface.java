bac3f84bd0c4e7cc38c61b42d11b810c217f2bc7
dua-dua <jonas_olseng@hotmail.com>
2015-01-27 14:07:13 +0100

    henter object fra database

--- app/src/main/java/com/example/dua/timeshiftproject/JavaScriptInterface.java
+++ app/src/main/java/com/example/dua/timeshiftproject/JavaScriptInterface.java
@@ -5,11 +5,19 @@ import android.content.Intent;
 import android.util.Log;
 import android.webkit.JavascriptInterface;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParsePush;
+import com.parse.ParseQuery;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.List;
+
+import bolts.Task;
 
 /**
  * Created by dualap on 26.01.2015.
@@ -40,7 +48,24 @@ public class JavaScriptInterface {
     }
 
     @JavascriptInterface
-    public boolean isLobby(){
+    public boolean isLobby(String lobbyId){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("LobbyList");
+
+        int numb = Integer.parseInt(lobbyId);
+
+        query.whereEqualTo("lobbyId",numb);
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject parseObject, ParseException e) {
+                if(parseObject==null){
+                    Log.v("test", "no such obj");
+                }
+                else{
+                    Log.v("test", "here`s the object");
+                }
+
+            }
+        });
         return true;
 
     }
