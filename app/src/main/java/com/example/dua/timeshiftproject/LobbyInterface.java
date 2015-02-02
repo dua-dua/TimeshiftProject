f3a8c98c7ced8366da4f8bc4c8e51d334319e094
dua-dua <jonas_olseng@hotmail.com>
2015-02-02 14:45:16 +0100

    Removed unused imports

--- app/src/main/java/com/example/dua/timeshiftproject/LobbyInterface.java
+++ app/src/main/java/com/example/dua/timeshiftproject/LobbyInterface.java
@@ -4,20 +4,11 @@ import android.app.Activity;
 import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
-
-import com.parse.FindCallback;
-import com.parse.GetCallback;
-import com.parse.Parse;
-import com.parse.ParseException;
 import com.parse.ParseInstallation;
-import com.parse.ParseObject;
 import com.parse.ParsePush;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.List;
 
 
@@ -37,24 +28,22 @@ public class LobbyInterface {
     public void playerReady() {
         ParseUser pUser = ParseUser.getCurrentUser();
         String name = "";
-        String installId = "";
         String channel = "";
 
         name = pUser.getUsername();
         JSONObject data = null;
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+        List channelList = installation.getList("channels");
+        channel = channelList.get(0).toString();
 
         try {
             data = new JSONObject();
             data.put("type","userReady");
             data.put("name",name);
+            data.put("channel",channel);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
-        installId = installation.getInstallationId();
-        List channelList = installation.getList("channels");
-        channel = channelList.get(0).toString();
 
         ParsePush push = new ParsePush();
         push.setChannel(channel);
@@ -62,6 +51,5 @@ public class LobbyInterface {
         push.sendInBackground();
 
         Log.v("tag", "User "+name+" is ready in channel "+channel);
-
     }
 }
