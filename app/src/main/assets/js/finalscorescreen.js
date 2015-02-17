f5f294a600ea21b857bd5ca0360fa824466578bc
dua-dua <jonas_olseng@hotmail.com>
2015-02-17 12:52:59 +0100

    FSS funker

---
+++ app/src/main/assets/js/finalscorescreen.js
@@ -0,0 +1,16 @@
+function setScoreText(name, score, index){
+    $("#player"+index+"").text(name+": "+score);
+}
+
+function setScorePlayer(score){
+    $("#yourScore").text("Your score: "+score);
+}
+
+function testSetText(){
+    window.FSSInterface.getTopFive();
+}
+
+function redirMenu(){
+    window.FSSInterface.toMenu();
+}
+
