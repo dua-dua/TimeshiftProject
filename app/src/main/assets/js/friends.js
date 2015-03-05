6d376d3b38e65c534083e2c8c515b481e0418e43
dua-dua <jonas_olseng@hotmail.com>
2015-03-05 15:51:23 +0100

    Add, accept, remove friend

--- app/src/main/assets/js/friends.js
+++ app/src/main/assets/js/friends.js
@@ -1,7 +1,3 @@
-function toMenu(){
-    window.friendInterface.toIndex();
-}
-
 function countRequests(){
     var size = $("#friendsList li").size()
     if(size == 0){
@@ -9,6 +5,34 @@ function countRequests(){
     }
 }
 
-$(
-    countRequests();
-)
+function addFriend(){
+    var receiver = "";
+    receiver = $("#friendName").val();
+    window.friendInterface.sendRequest(receiver);
+    $("#addNotify").text("Friend added");
+    $("#addNotify").fadeOut(2000);
+}
+
+function addToLi(name){
+    $("#friendsList").append("<li><span id="+name+">"+name+"</span><button>+</button></li>");
+    /*test mer her
+    list.append("<button>ButtonTest</button>");
+
+    list.on("click", "button", function(){
+        getFeed('http://open.live.bbc.co.uk/weather/feeds/en/PA2/3dayforecast.rss, showFeedResults');
+    });
+    */
+}
+
+function getRequests(){
+    window.friendInterface.getRequests();
+}
+
+function acceptRequest(){
+    $("adf").text("click");
+    window.friendInterface.acceptRequest("qwer");
+}
+
+$(function(){
+    getRequests();
+});
--- app/src/main/java/com/example/dua/timeshiftproject/interfaces/FriendInterface.java
+++ app/src/main/java/com/example/dua/timeshiftproject/interfaces/FriendInterface.java
@@ -7,6 +7,20 @@ import android.webkit.WebView;
 
 import com.example.dua.timeshiftproject.activites.FriendActivity;
 import com.example.dua.timeshiftproject.activites.MenuActivity;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.ParsePush;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by dualap on 03.03.2015.
@@ -22,10 +36,82 @@ public class FriendInterface {
     }
 
     @JavascriptInterface
+    public void sendRequest(final String receiver){
+        //Send request
+        final String current = ParseUser.getCurrentUser().getUsername().toString();
+        ParseObject friendRequest = new ParseObject("FriendRequest");
+        friendRequest.put("sender", current);
+        friendRequest.put("receiver", receiver);
+        friendRequest.saveInBackground();
+        addToMyList(receiver);
+    }
+
+    public void addToMyList(String name){
+        Log.v("friendreq", "entered addtomylist "+name);
+        //Add friend to my list
+        List friendArray = ParseUser.getCurrentUser().getList("friends");
+        if(friendArray == null){
+            String[] friends = {name};
+            ParseUser.getCurrentUser().put("friends", Arrays.asList(friends));
+            ParseUser.getCurrentUser().saveInBackground();
+        }else{
+            friendArray.add(name);
+            ParseUser.getCurrentUser().put("friends", friendArray);
+            ParseUser.getCurrentUser().saveInBackground();
+        }
+        Log.v("friendreq", "finished addtomylist");
+    }
+
+    @JavascriptInterface
+    public void removeRequest(String name){
+        Log.v("friendreq", "entered removerequest"+name);
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("FriendRequest");
+        String current = ParseUser.getCurrentUser().getUsername();
+        query.whereEqualTo("sender", name);
+        query.whereEqualTo("receiver", current);
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject parseObject, ParseException e) {
+                parseObject.deleteInBackground();
+            }
+        });
+        Log.v("friendreq", "finished removerequest");
+    }
+
+    @JavascriptInterface
+    public void getRequests(){
+        String current = ParseUser.getCurrentUser().getUsername().toString();
+        Log.v("friendreq", "inside");
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("FriendRequest");
+        query.whereEqualTo("receiver", current);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> parseObjects, ParseException e) {
+                for(int i = 0; i < parseObjects.size(); i++){
+                    addToHTMLList(parseObjects.get(i).getString("sender"));
+                }
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void acceptRequest(String name){
+        Log.v("friendreq", "entered with "+name);
+        addToMyList(name);
+        removeRequest(name);
+    }
+
+    @JavascriptInterface
     public void toIndex(){
         Intent intent = new Intent(activity, MenuActivity.class);
         activity.startActivity(intent);
         activity.finish();
+    }
+
+    @JavascriptInterface
+    public void addToHTMLList(String name){
+        Log.v("friendreq", "added "+name);
+        webView.loadUrl("javascript:addToLi(\""+name+"\")");
     }
 
 }
